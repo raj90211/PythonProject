498b71118b7c96e0dcf73b5c78cf1a6250e367af
Raj Kumar <raj90211@gmail.com>
2020-09-02 16:00:19 +0530

    InitialCommit

---
+++ Main.py
@@ -0,0 +1,21 @@
+
+from Mathfunction import *
+
+#funcion calls only
+# create anther files for defining functins
+
+
+x=input('Enter Value of x\n')
+y=input('Enter Value of y\n')
+
+add = Addition(x,y)
+print('Addition function=',add)
+
+Subtraction(x,y)
+print('Subtraction function=',Subtraction(x,y))
+
+Multiplication(x,y)
+print('Multiplication function=',Multiplication(x,y))
+
+Division(x,y)
+print('Division function=',Division(x,y))
---
+++ Mathfunction.py
@@ -0,0 +1,18 @@
+def Addition(x,y):
+    #print('Addition function')
+    #print(round(float(x)+float(y),1))
+    return round(float(x) + float(y), 1)
+
+def Subtraction(x,y):
+    #print('Subtraction function')
+    #print(round(float(x)-float(y),1))
+    return round(float(x) - float(y), 1)
+def Multiplication(x,y):
+    #print('Multiplication function')
+    #print(round(float(x)*float(y),1))
+    return round(float(x) * float(y), 1)
+
+def Division(x,y):
+    #print('Division function')
+    #print(round(float(x)/float(y),1))
+    return round(float(x) / float(y), 1)
